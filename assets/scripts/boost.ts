e2654608cefd03294b1b7f15ddc46590c96bd933
liangly-mac <liangly-mac@gmail.com>
2026-05-07 22:44:10 +0800

    feat: 添加Match3游戏场景、UI资源及屏幕适配逻辑
    添加Match3游戏的主场景和UI资源，包括背景图、游戏元素图标和预制体
    实现boost脚本用于动态加载Match3游戏包并适配不同屏幕分辨率
    配置项目设计分辨率为750x1334，设置Match3资源为独立资源包

---
+++ assets/scripts/boost.ts
@@ -0,0 +1,75 @@
+import { _decorator, assetManager, Component, instantiate, Node, Prefab, ResolutionPolicy, resources, screen, Size, UITransform, view } from 'cc';
+const { ccclass, property } = _decorator;
+
+
+/** 
+ * 画布的标准化尺寸，就是之前说的
+ * iPad 设备中的画布尺寸 = 1001 x 1334 (其中 1001 ≈ 1668/1.6672)
+ * iPhone16设备中的画布尺寸 = 750 x1626（其中 1626 = 2556/1.572）
+ */
+export const G_VIEW_SIZE = new Size(0, 0);
+
+@ccclass('boost')
+export class boost extends Component {
+    // @property(Prefab) private match3Prefab: Prefab = null;
+
+    start() {
+        const WIN_SIZE_W = screen.windowSize.width;
+        const WIN_SIZE_H = screen.windowSize.height;
+        let isScreenWidthLarger = this.adapterScreen();
+        if (isScreenWidthLarger) {
+            screen.windowSize = new Size(WIN_SIZE_W + 1, WIN_SIZE_H);
+            screen.windowSize = new Size(WIN_SIZE_W, WIN_SIZE_H);
+        }
+
+        // this.scheduleOnce(() => {
+        //     let match3Node = instantiate(this.match3Prefab);
+        //     this.node.addChild(match3Node);
+        //     match3Node.getComponent(UITransform).setContentSize(G_VIEW_SIZE.clone());
+        // }, 1);
+        this.scheduleOnce(() => {
+            assetManager.loadBundle("Match3BN", (e, bundle) => {
+                bundle.load("Match3UI", Prefab, (error, prefab: Prefab) => {
+                    if (error) {
+                        console.error(error);
+                        return;
+                    }
+                    let match3Node = instantiate(prefab);
+                    this.node.addChild(match3Node);
+                    match3Node.getComponent(UITransform).setContentSize(G_VIEW_SIZE.clone());
+                })
+            })
+        }, 1)
+    }
+
+    update(deltaTime: number) {
+        
+    }
+
+    adapterScreen() {
+        let resolutionPolicy: ResolutionPolicy = view.getResolutionPolicy();
+        let designSize = view.getDesignResolutionSize();
+        let frameSize = screen.windowSize;
+        let frameW = frameSize.width;
+        let frameH = frameSize.height;
+        /** 是否是屏幕更宽 */
+        const isScreenWidthLarger = (frameW / frameH) > (designSize.width / designSize.height);
+        let targetResolutionPolicy = isScreenWidthLarger ? ResolutionPolicy.FIXED_HEIGHT : ResolutionPolicy.FIXED_WIDTH;
+        if (targetResolutionPolicy !== resolutionPolicy.getContentStrategy().strategy) {
+            /** 保证设计分辨率的内容都能显示出来 */
+            view.setDesignResolutionSize(designSize.width, designSize.height, targetResolutionPolicy);
+        }
+        /** 实际的尺寸会和设计分辨率在一个维度，但是宽或高更大 */
+        if (isScreenWidthLarger) {
+            G_VIEW_SIZE.width = Math.ceil(designSize.height * frameSize.width / frameSize.height);
+            G_VIEW_SIZE.height = designSize.height;
+        } else {
+            G_VIEW_SIZE.width = designSize.width;
+            G_VIEW_SIZE.height = Math.ceil(designSize.width * frameSize.height / frameSize.width);
+        }
+        console.log(`屏幕${isScreenWidthLarger ? "更宽, 高度适配" : "更高, 宽度适配"} 设计分辨率比例下的屏幕尺寸: ${G_VIEW_SIZE.width}x${G_VIEW_SIZE.height}`);
+
+        return isScreenWidthLarger;
+    }   
+}
+
